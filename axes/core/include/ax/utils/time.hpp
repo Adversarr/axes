a92282d315bda1a4b0a2879c0a0de3f723601036
Adversarr <1444781838@qq.com>
2024-05-11 12:53:17 +0800

    Remove long and unused comments

--- axes/core/include/ax/utils/time.hpp
+++ axes/core/include/ax/utils/time.hpp
@@ -25,9 +25,7 @@ public:
   TimerRegistry() = default;
   ~TimerRegistry() {
     for (const auto& [name, desc] : total_durations_) {
-      std::cout << "Timer: " << name
-                << "\n- total: " << desc.total_
-                << "\n- count: " << desc.cnt_
+      std::cout << "Timer: " << name << "\n- total: " << desc.total_ << "\n- count: " << desc.cnt_
                 << "\n- avg:   " << (desc.total_ / desc.cnt_) << '\n';
     }
   }
@@ -37,13 +35,13 @@ public:
     idx cnt_ = 0;
   };
 
-  void AddDuration(const std::string& name, Duration duration) {
+  inline void AddDuration(const char* name, Duration duration) {
     if (total_durations_.find(name) == total_durations_.end()) {
       auto [it, _] = total_durations_.emplace(name, Desc{});
       it->second.total_ = duration;
       it->second.cnt_ = 1;
     } else {
-      auto &cur = total_durations_[name];
+      auto& cur = total_durations_[name];
       cur.total_ += duration;
       cur.cnt_ += 1;
     }
@@ -56,21 +54,28 @@ private:
 // Use RAII to measure time
 class Timer {
 public:
-  Timer(const std::string& name) : name_(name), start_time_(absl::Now()) {}
+  inline Timer(const char* name, bool en = true) : name_(name), en_(en) {
+    if (en_) {
+      start_time_ = absl::Now();
+    }
+  }
 
-  ~Timer() {
-    absl::Duration duration = absl::Now() - start_time_;
-    if (auto reg = try_get_resource<TimerRegistry>(); reg == nullptr) {
-      reg = &add_resource<TimerRegistry>();
-      reg->AddDuration(name_, duration);
-    } else {
-      reg->AddDuration(name_, duration);
+  inline ~Timer() {
+    if (en_) {
+      absl::Duration duration = absl::Now() - start_time_;
+      if (auto reg = try_get_resource<TimerRegistry>(); reg == nullptr) {
+        reg = &add_resource<TimerRegistry>();
+        reg->AddDuration(name_, duration);
+      } else {
+        reg->AddDuration(name_, duration);
+      }
     }
   }
 
 private:
-  const std::string name_;
-  const absl::Time start_time_;
+  const char* name_;
+  absl::Time start_time_;
+  const bool en_;
 };
 
 #define AX_TIMEIT(name) ::ax::utils::Timer timer_do_not_use(#name)
--- axes/fem/source/fem/timestepper/quasi_newton.cpp
+++ axes/fem/source/fem/timestepper/quasi_newton.cpp
@@ -4,12 +4,8 @@
 
 #include "ax/fem/laplace_matrix.hpp"
 #include "ax/math/linsys/preconditioner/Diagonal.hpp"
-#include "ax/math/linsys/preconditioner/IncompleteCholesky.hpp"
 #include "ax/optim/common.hpp"
 #include "ax/optim/optimizers/lbfgs.hpp"
-#include "ax/optim/spsdm/eigenvalue.hpp"
-#include "ax/utils/iota.hpp"
-#include "ax/utils/time.hpp"
 
 namespace ax::fem {
 
@@ -19,117 +15,6 @@ template <idx dim> Status fem::Timestepper_QuasiNewton<dim>::Initialize() {
   solver_->SetPreconditioner(std::make_unique<math::PreconditionerDiagonal>());
   AX_RETURN_OK();
 }
-
-// template <idx dim> Status fem::Timestepper_QuasiNewton<dim>::Step(real dt) {
-//   // Get the mesh
-//   AX_TIME_FUNC();
-//   auto &mesh = this->GetMesh();
-//   auto &elasticity = this->GetElasticity();
-//   elasticity.SetLame(this->u_lame_);
-//   auto &velocity = this->velocity_;
-//   auto const &mass_matrix = this->mass_matrix_;
-//   math::vec2r lame = this->u_lame_;
-
-//   idx n_vert = mesh.GetNumVertices();
-
-//   // Setup the NonLinear Problem.
-//   optim::OptProblem problem;
-//   math::fieldr<dim> const x_cur = mesh.GetVertices();
-//   math::vecxr const v_flat = velocity.reshaped();
-//   math::vecxr const a_flat = this->ext_accel_.reshaped();
-//   math::vecxr y = dt * v_flat + dt * dt * a_flat;
-//   mesh.FilterVector(y, true);
-//   for (idx i = 0; i < n_vert; ++i) {
-//     for (idx d = 0; d < dim; ++d) {
-//       if (mesh.IsDirichletBoundary(i, d)) {
-//         y(i * dim + d) = mesh.GetBoundaryValue(i, d) - x_cur(d, i);
-//       }
-//     }
-//   }
-
-//   math::vecxr eacc = this->ext_accel_.reshaped();
-//   mesh.FilterVector(eacc, true);
-//   real max_tol = (mass_matrix * math::vecxr::Ones(n_vert * dim)).maxCoeff() + math::epsilon<real>;
-//   problem
-//       .SetEnergy([&](math::vecxr const &dx) -> real {
-//         AX_TIMEIT("Eval Energy");
-//         math::fieldr<dim> x_new = dx.reshaped(dim, n_vert) + x_cur;
-//         elasticity.Update(x_new, ElasticityUpdateLevel::kEnergy);
-//         elasticity.UpdateEnergy();
-//         real elasticity_energy = elasticity.GetEnergyOnElements().sum() * dt * dt;
-//         math::vecxr x_y = dx - y;
-//         real kinematic_energy = x_y.dot(mass_matrix * x_y) * 0.5;
-//         return elasticity_energy + kinematic_energy;
-//       })
-//       .SetGrad([&](math::vecxr const &dx) -> math::vecxr {
-//         AX_TIMEIT("Eval Grad");
-//         math::fieldr<dim> x_new = dx.reshaped(dim, n_vert) + x_cur;
-//         elasticity.Update(x_new, ElasticityUpdateLevel::kStress);
-//         elasticity.UpdateStress();
-//         elasticity.GatherStressToVertices();
-//         math::fieldr<dim> neg_force = elasticity.GetStressOnVertices();
-//         math::vecxr grad_kinematic = mass_matrix * (dx - y);
-//         math::vecxr grad_elasticity = neg_force.reshaped() * (dt * dt);
-//         math::vecxr grad = grad_elasticity + grad_kinematic;
-//         mesh.FilterVector(grad, true);
-//         return grad;
-//       })
-//       .SetSparseHessian([&](math::vecxr const &dx) -> math::sp_matxxr {
-//         AX_TIMEIT("Eval Sparse Hessian");
-//         math::fieldr<dim> x_new = dx.reshaped(dim, n_vert) + x_cur;
-//         elasticity.Update(x_new, ElasticityUpdateLevel::kHessian);
-//         elasticity.UpdateHessian(true);
-//         elasticity.GatherHessianToVertices();
-//         auto stiffness = elasticity.GetHessianOnVertices();
-//         math::sp_matxxr hessian = mass_matrix + (dt * dt) * stiffness;
-//         mesh.FilterMatrixFull(hessian);
-//         return hessian;
-//       })
-//       .SetConvergeGrad([&](const math::vecxr &, const math::vecxr &grad) -> real {
-//         real rv = math::abs(grad).maxCoeff() / max_tol / (dt * dt);
-//         return rv;
-//       })
-//       .SetConvergeVar(nullptr);
-
-//   optim::Lbfgs lbfgs;
-//   lbfgs.SetTolGrad(0.02);
-//   lbfgs.SetMaxIter(1000);
-
-//   if (strategy_ == LbfgsStrategy::kHard) {
-//     math::LinsysProblem_Sparse problem_sparse;
-//     problem_sparse.A_ = problem.EvalSparseHessian(y);
-//     AX_CHECK_OK(solver_->Analyse(problem_sparse));
-//   }
-
-//   if (strategy_ == LbfgsStrategy::kLaplacian) {
-//     lbfgs.SetApproxSolve([&](math::vecxr const &g) -> math::vecxr {
-//       auto approx = solver_->Solve(g, g * dt * dt);
-//       AX_CHECK_OK(approx);
-//       return std::move(approx->solution_);
-//     });
-//   } else if (strategy_ == LbfgsStrategy::kHard) {
-//     lbfgs.SetApproxSolve([&](math::vecxr const &g) -> math::vecxr {
-//       auto approx = solver_->Solve(g, g * dt * dt);
-//       AX_CHECK_OK(approx);
-//       return std::move(approx->solution_);
-//     });
-//   }
-
-//   auto result = lbfgs.Optimize(problem, y);
-
-//   if (!result.ok()) {
-//     AX_LOG(WARNING) << "LBFGS iteration failed to compute! (not a convergency problem.)";
-//     return result.status();
-//   } else if (!(result->converged_grad_ || result->converged_var_)) {
-//     AX_LOG(ERROR) << "LBFGS iteration failed to converge!";
-//   }
-//   AX_LOG(WARNING) << "#Iter: " << result->n_iter_ << " iterations.";
-//   math::fieldr<dim> x_new = result->x_opt_.reshaped(dim, mesh.GetNumVertices()) + x_cur;
-//   velocity = (x_new - x_cur) / dt;
-//   AX_RETURN_NOTOK(mesh.SetVertices(mesh.GetVertices() + velocity * dt));
-//   AX_RETURN_OK();
-// }
-
 template <idx dim> void fem::Timestepper_QuasiNewton<dim>::UpdateSolverLaplace() {
   this->integration_scheme_->SetDeltaT(dt_back_);
   math::LinsysProblem_Sparse problem_sparse;

91764d474de9c5609ef147e020cda79a999e7c84
Adversarr <1444781838@qq.com>
2024-05-14 12:57:05 +0800

    Add minor definition for xpbd

--- axes/xpbd/include/ax/xpbd/common.hpp
+++ axes/xpbd/include/ax/xpbd/common.hpp
@@ -1,11 +1,33 @@
 /**
  * @brief Mass spring is also an inexact method.
- * 
+ *
  */
 
 #pragma once
+#include "ax/core/common.hpp"
 #include "ax/core/config.hpp"
 #include "ax/math/common.hpp"
 namespace ax::xpbd {
 
-}
+template <idx dim> class ConstraintBase {
+public:
+};
+
+template <idx dim> class SolverBase {
+public:
+};
+
+template <idx dim> class GlobalServer {
+public:
+  // We only care about 1st order euler.
+  math::field1i identifiers_;
+  math::fieldr<dim> vertices_;
+  math::fieldr<dim> velocities_;
+  math::fieldr<dim> ext_forces_;
+
+  // Constraints.
+  List<UPtr<ConstraintBase<dim>>> constraints_;
+  UPtr<SolverBase<dim>> solver_;
+};
+
+}  // namespace ax::xpbd
